85c8b2308ceedf062cef60ddaed0b7dc3d0a1e4a
Tommi Tuominen <ttuominen@outlook.com>
2022-05-24 22:39:32 +0300

    Draft out the repo pattern

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const UserRepo = require('../repos/user-repo');
 
 const router = express.Router();
 
-router.get('/users', async (req, res) => {});
+router.get('/users', async (req, res) => {
+  const users = await UserRepo.find();
+
+  res.send(users);
+});
+
 router.get('/users/:id', async (req, res) => {});
 router.post('/users', async (req, res) => {});
 router.put('/users/:id', async (req, res) => {});
