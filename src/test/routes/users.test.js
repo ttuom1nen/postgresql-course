964e81bdd61ca17171a0f29b0e927d04b57ad300
Tommi Tuominen <ttuominen@outlook.com>
2022-07-02 23:37:01 +0300

    Add users test

--- src/test/routes/users.test.js
+++ src/test/routes/users.test.js
@@ -2,65 +2,20 @@ const request = require('supertest');
 const buildApp = require('../../app');
 const UserRepo = require('../../repos/user-repo');
 const pool = require('../../pool');
+const Context = require('../Context');
 
-const { randomBytes } = require('crypto');
-const { default: migrate } = require('node-pg-migrate');
-const format = require('pg-format');
+let context;
 
 beforeAll(async () => {
-  // Random role name to connect to PG as
-  // Be sure to start with a letter
-  const roleName = 'a' + randomBytes(4).toString('hex');
-
-  await pool.connect({
-    host: 'localhost',
-    port: 5432,
-    database: 'socialnetwork-test',
-    user: 'postgres',
-    password: 'password',
-  });
-
-  // Create a new role
-  await pool.query(`
-  CREATE ROLE ${roleName} WITH LOGIN PASSWORD '${roleName}'
-`);
-
-  // Create a schema with the same name
-  await pool.query(`
-  CREATE SCHEMA ${roleName} AUTHORIZATION ${roleName};
-`);
-
-  // Disconnect from PG
-  await pool.close();
-
-  // Run migrations in the new schema
-  await migrate({
-    schema: roleName,
-    direction: 'up',
-    log: () => {},
-    noLock: true,
-    dir: 'migrations',
-    databaseUrl: {
-      host: 'localhost',
-      port: 5432,
-      database: 'socialnetwork-test',
-      user: roleName,
-      password: roleName,
-    },
-  });
-
-  // Connect to PG as the new role
-  await pool.connect({
-    host: 'localhost',
-    port: 5432,
-    database: 'socialnetwork-test',
-    user: roleName,
-    password: roleName,
-  });
+  context = await Context.build();
 });
 
+beforeEach(async () => {
+  await context.reset();
+})
+
 afterAll(() => {
-  return pool.close();
+  return context.close();
 });
 
 it('create a user', async () => {
